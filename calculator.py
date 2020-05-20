28d3073330311153a27fb66a1402c5af56bc0850
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-20 11:43:45 +0800

    Implemented basic operation

--- calculator.py
+++ calculator.py
@@ -3,5 +3,16 @@ print('This is a calculator')
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
 
-print('First num: ' + str(num1))
-print('Second num: ' + str(num2))
+# print('First num: ' + str(num1))
+# print('Second num: ' + str(num2))
+
+oper = input('Enter operator: ')
+
+if (oper == '+'):
+    print('Result is equal to :' + str(num1 + num2))
+elif (oper == '-'):
+    print('Result is equal to :' + str(num1 - num2))
+elif (oper == '*'):
+    print('Result is equal to :' + str(num1 * num2))
+elif (oper == '/'):
+    print('Result is equal to :' + str(num1 / num2))

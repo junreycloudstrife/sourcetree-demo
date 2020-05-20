1e59d835d27c582c4e60413eaf7a850f8c1d3257
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-20 11:47:26 +0800

    Updated the header comment

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
-print('This is a calculator')
+print('This is basic calculator with basic operations')
+print('1. Enter the first number')
+print('2. Enter the second number')
+print('3. Enter the operator')
 
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
-
-# print('First num: ' + str(num1))
-# print('Second num: ' + str(num2))
 
 oper = input('Enter operator: ')
 

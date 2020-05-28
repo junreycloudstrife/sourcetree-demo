d89270809632bece3123ff666fb1b53c48c5841e
junreycloudstrife <junrey.cloudstrife@gmail.com>
2020-05-28 09:43:44 +0800

    Implemented function and dictionary for the operations

--- calculator.py
+++ calculator.py
@@ -1,7 +1,21 @@
-print('This is a calculator')
+print('This is a basic calculator')
+print('Enter the first number')
+print('Enter the second number')
+print('Enter the operator')
+
+def operations(operator):
+    switcher = {
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        '/': num1 / num2,
+        '%': num1 % num2
+    }
+    return switcher.get(operator, "Invalid operator")
 
 num1 = int(input("First number: "))
 num2 = int(input("Second number: "))
+oper = input("Enter operator: ")
+result = operations(oper)
+print("Result is equal to " + str(result))
 
-print('First num: ' + str(num1))
-print('Second num: ' + str(num2))
